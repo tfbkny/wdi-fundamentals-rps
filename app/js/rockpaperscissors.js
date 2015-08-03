c67db824f45dbeb794e5c249c6fa16aa507f0408
toti <toti@MegaMacPro.local>
2015-08-03 03:01:03 -0400

    fixed code intentation

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -37,29 +37,29 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
   var winner;
-	// Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+  // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
   // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
   // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
   /* YOUR CODE HERE */
-	
-	// Draw, Player wins, Computer wins
-	if (playerMove == computerMove) {
-		winner = 'tie';
-	}else if (playerMove == 'rock' && computerMove == 'scissors' || playerMove == 'scissors' && computerMove == 'paper' || playerMove == 'paper' && computerMove == 'rock' ) {
-		winner == 'player';
-	}else{
-		winner == 'computer';
-	} 
-	return winner;
+  
+  // Draw, Player wins, Computer wins
+  if (playerMove == computerMove) {
+    winner = 'tie';
+  }else if (playerMove == 'rock' && computerMove == 'scissors' || playerMove == 'scissors' && computerMove == 'paper' || playerMove == 'paper' && computerMove == 'rock' ) {
+    winner == 'player'; 
+  }else{
+    winner == 'computer';
+  } 
+  return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    
-		return [playerWins, computerWins];
+  console.log("Let's play Rock, Paper, Scissors");
+  var playerWins = 0;
+  var computerWins = 0;
+  // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+  /* YOUR CODE HERE */
+
+  return [playerWins, computerWins];
 }
 
